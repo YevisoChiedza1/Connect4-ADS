80e89ee3008b584cc5fb66825f4b69308ff71e8b
Chiedza <yevisochiedza@gmail.com>
2026-04-06 13:40:51 +0100

    Added ANSI Color Codes for the Terminal

---
+++ src/GameSettings.java
@@ -0,0 +1,11 @@
+/**
+ * Main class for the Connect Four game colour components.
+ * Handles ANSI Color Codes for the Terminal
+ */
+public class GameSettings {
+    public static final String RESET = "\u001B[0m";
+	public static final String RED = "\u001B[31m";    // For Player R
+	public static final String YELLOW = "\u001B[33m"; // For Player Y
+	public static final String CYAN = "\u001B[36m";   // For UI Elements
+	public static final String GREEN = "\u001B[32m";  // For Wins/Success
+}
