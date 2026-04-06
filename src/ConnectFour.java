4667d86c5b2b94eb21ee959fb985b7584c109c42
Chiedza <yevisochiedza@gmail.com>
2026-04-06 12:00:10 +0100

    Added entry point of the game and ASCII welcome message

---
+++ src/ConnectFour.java
@@ -0,0 +1,18 @@
+public class ConnectFour {
+    public static void main(String[] args) {
+        printWelcomeMessage();
+    }
+
+    public static void printWelcomeMessage() {
+        System.out.println("====================================================");
+        System.out.println("  _____                               _     _  _   ");
+        System.out.println(" / ____|                             | |   | || |  ");
+        System.out.println("| |     ___  _ __  _ __   ___  ___  | |_  | || |_ ");
+        System.out.println("| |    / _ \\| '_ \\| '_ \\ / _ \\/ __| | __| |__   _|");
+        System.out.println("| |___| (_) | | | | | | |  __/ (__  | |_     | |  ");
+        System.out.println(" \\_____\\___/|_| |_|_| |_|\\___|\\___|  \\__|    |_|  ");
+        System.out.println("====================================================");
+        System.out.println("                Welcome to Connect 4                 ");
+        System.out.println("====================================================\n");
+    }
+}
